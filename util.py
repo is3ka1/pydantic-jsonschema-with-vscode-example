c4091dff344c7c5b63b63427dec25f6e9a724ebb
Isekai-Seikatsu <brian@zuso.tw>
2022-05-29 20:30:39 +0800

    Fix schema file name

--- util.py
+++ util.py
@@ -1,9 +1,10 @@
 import pathlib
+
 from pydantic_jsonschema_auto_completion.config import get_app_config_json_schema
 
 
 def gen_json_schema():
-    with open(pathlib.Path().absolute() / "schema.yaml", "w") as schema_file:
+    with open(pathlib.Path().absolute() / "schema.json", "w") as schema_file:
         schema_file.write(get_app_config_json_schema())
 
 
